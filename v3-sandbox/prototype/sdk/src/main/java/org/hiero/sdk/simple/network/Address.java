e75e98367772ea38c9580b4013a7d66255d0a0f2
Hendrik Ebbers <hendrik.ebbers@web.de>
2025-11-05 14:45:52 +0100

    Network record and address checksum check starten
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- v3-sandbox/prototype/sdk/src/main/java/org/hiero/sdk/simple/network/Address.java
+++ v3-sandbox/prototype/sdk/src/main/java/org/hiero/sdk/simple/network/Address.java
@@ -2,18 +2,52 @@ package org.hiero.sdk.simple.network;
 
 import org.jspecify.annotations.NonNull;
 
+/**
+ *  An address on a Hiero network.
+ */
 public interface Address {
 
+    /**
+     *  Returns the shard number.
+     *
+     *  @return the shard number
+     */
     long shard();
 
+    /**
+     *  Returns the realm number.
+     *
+     * @return the realm number
+     */
     long realm();
 
+    /**
+     *  Returns the num.
+     *
+     * @return the num
+     */
     long num();
 
+    /**
+     *  Returns the checksum.
+     *
+     * @return the checksum
+     */
     @NonNull String checksum();
 
+    /**
+     *  Returns true if the checksum is valid for the given network.
+     *
+     * @param network the network to validate against
+     * @return true if the checksum is valid for the given network
+     */
     boolean validateChecksum(@NonNull Network network);
 
+    /**
+     *  Returns a human readable string representation of this address.
+     *
+     * @return a human readable string representation of this address
+     */
     @NonNull
     default String toHumanReadbleString() {
         return shard() + "." + realm() + "." + num();
